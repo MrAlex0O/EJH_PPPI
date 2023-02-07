c94f6cf942238fabf4f60caf6f1e863ef02661cc
Alexey Istyagin <ist_75@mail.ru>
2023-02-07 09:51:17 +0300

    feat(logic): add writeService interfaces

---
+++ Program/Logic/WriteServices/Interfaces/ITeacherWriteService.cs
@@ -0,0 +1,17 @@
+﻿using DataBase.Models;
+using Logic.DTOs.Teacher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.WriteServices.Interfaces
+{
+    public interface ITeacherWriteService
+    {
+        public void Add(Guid personId);
+        public Teacher Update(Guid Id, UpdateTeacherRequest updateTeacherRequest);
+        public Teacher Delete(Guid id);
+    }
+}
