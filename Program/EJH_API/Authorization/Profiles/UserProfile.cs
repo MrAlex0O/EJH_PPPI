1f1e5554b87c94eee18d95b0c81699ea678dfe35
Alexey Istyagin <ist_75@mail.ru>
2023-02-07 10:08:14 +0300

    feat(api): add user DTOs and profile

---
+++ Program/EJH_API/Authorization/Profiles/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using API.Authorization.Models.Users;
+using AutoMapper;
+using DataBase.Models;
+
+namespace API.Authorization.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, AuthResponse>();
+            CreateMap<Person, AuthResponse>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null)); ;
+            CreateMap<AuthResponse, Person>();
+            CreateMap<RegisterRequest, User>();
+            CreateMap<UpdateRequest, User>();
+            CreateMap<RegisterRequest, Person>();
+            CreateMap<UpdateRequest, Person>();
+            CreateMap<Role, RoleResponse>();
+        }
+    }
+}
