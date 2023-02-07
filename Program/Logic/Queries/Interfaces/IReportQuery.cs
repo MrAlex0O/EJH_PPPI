e7ba5e45696eeabf9ccf1d62d55bf2a42e2ac282
Alexey Istyagin <ist_75@mail.ru>
2023-02-07 09:55:51 +0300

    feat(logic): add queries interfaces

---
+++ Program/Logic/Queries/Interfaces/IReportQuery.cs
@@ -0,0 +1,22 @@
+﻿using Logic.DTOs.Report;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Queries.Interfaces
+{
+    public interface IReportQuery
+    {
+        public List<GetDisciplineVisitsReportResponse> GetDisciplineVisits(Guid disciplineId);
+        public List<GetStudentVisitsResponse> GetDisciplineVisitsByStudentId(Guid studentId);
+
+        public List<GetStudentVisitsResponse> GetStudentVisitsByDay(GetStudentVisitByDayRequest request);
+
+
+        public List<GetStudentVisitsResponse> GetStudentVisitsByInterval(GetStudentVisitsByIntervalRequest request);
+
+
+    }
+}
